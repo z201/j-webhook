dbbe6ef4072bcb08b7ef19b9a9d588a1143558c3
z201 <z201.coding@gmail.com>
2021-03-03 00:58:23 +0800

    初始化项目

---
+++ src/main/java/z201/cn/webhook/starter/MainVerticle.java
@@ -0,0 +1,33 @@
+package z201.cn.webhook.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+
+public class MainVerticle extends AbstractVerticle {
+
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    HttpServer server = vertx.createHttpServer();
+    Router router = Router.router(vertx);
+    router
+      .get("/info")
+      .respond(
+        ctx -> ctx
+          .response()
+          .putHeader("Content-Type", "text/plain")
+          .end("hello world!"));
+    server.requestHandler(router).listen(8888, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+
+
+}
---
+++ src/test/java/z201/cn/webhook/starter/TestMainVerticle.java
@@ -0,0 +1,37 @@
+package z201.cn.webhook.starter;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+public class TestMainVerticle {
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void verticleDeployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    testContext.completeNow();
+  }
+
+  @Test
+  void helloHttp(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    HttpClient client = vertx.createHttpClient();
+    client.request(HttpMethod.GET, 8888, "localhost", "/info")
+      .compose(req -> req.send().compose(HttpClientResponse::body))
+      .onComplete(testContext.succeeding(buffer -> testContext.verify(() -> {
+        System.out.println(buffer.toString());
+        testContext.completeNow();
+      })));
+  }
+
+}
