68a25c9621234d48e1567f46816a4e89d9e6f01b
z201 <z201.coding@gmail.com>
2021-03-03 22:48:06 +0800

    代码进度更新

--- src/main/java/z201/cn/webhook/starter/MainVerticle.java
+++ src/main/java/z201/cn/webhook/starter/MainVerticle.java
@@ -4,6 +4,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -11,8 +12,12 @@ public class MainVerticle extends AbstractVerticle {
   public void start(Promise<Void> startPromise) throws Exception {
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
+    router.route().handler(BodyHandler.create());
     router
       .post("/info")
+      .handler(ctx -> {
+        System.out.println(ctx.getBodyAsJson().toString());
+      })
       .respond(
         ctx -> ctx
           .response()
