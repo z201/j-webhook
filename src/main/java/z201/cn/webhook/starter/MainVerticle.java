2592de013addb251b51eb27c6b3c16fa295d60b9
z201 <z201.coding@gmail.com>
2021-03-04 00:30:34 +0800

    代码进度更新

--- src/main/java/z201/cn/webhook/starter/MainVerticle.java
+++ src/main/java/z201/cn/webhook/starter/MainVerticle.java
@@ -1,10 +1,13 @@
 package z201.cn.webhook.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
+
+import java.util.Map;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -12,10 +15,14 @@ public class MainVerticle extends AbstractVerticle {
   public void start(Promise<Void> startPromise) throws Exception {
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
-    router.route().handler(BodyHandler.create());
     router
       .post("/info")
       .handler(ctx -> {
+        HttpServerRequest httpServerRequest = ctx.request();
+        MultiMap multiMap = httpServerRequest.headers();
+        for (Map.Entry<String, String> stringStringEntry : multiMap) {
+          System.out.println(stringStringEntry.getKey() + " " + stringStringEntry.getValue());
+        }
         System.out.println(ctx.getBodyAsJson().toString());
       })
       .respond(
@@ -23,6 +30,7 @@ public class MainVerticle extends AbstractVerticle {
           .response()
           .putHeader("Content-Type", "text/plain")
           .end("hello world!"));
+
     server.requestHandler(router).listen(8888, http -> {
       if (http.succeeded()) {
         startPromise.complete();
