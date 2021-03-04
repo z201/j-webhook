a147c47a165d91155815ba98042fb2db52df4b09
z201 <z201.coding@gmail.com>
2021-03-05 01:04:41 +0800

    代码进度更新

---
+++ src/main/java/z201/cn/webhook/starter/ConfKey.java
@@ -0,0 +1,16 @@
+package z201.cn.webhook.starter;
+
+/**
+ * @author z201.coding@gmail.com
+ * @date 3/4/21
+ **/
+public interface ConfKey {
+  String PORT_KEY = "http.port";
+  String GITHUB_SECRET_KEY = "github.secret";
+  String GITHUB_REPOSITORY_KEY = "github.repository";
+  String GITHUB_REPOSITORY_SHH_URL_KEY = "ssh_url";
+  String REPOSITORY_KEY = "repository";
+  String SSH_URL_KEY = "ssh.url";
+  String SCRIPT_PATH_KEY = "script.path";
+  String NAME_KEY = "name";
+}
--- src/main/java/z201/cn/webhook/starter/MainVerticle.java
+++ src/main/java/z201/cn/webhook/starter/MainVerticle.java
@@ -1,15 +1,31 @@
 package z201.cn.webhook.starter;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.file.FileReader;
+import cn.hutool.core.util.HexUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.digest.HMac;
+import cn.hutool.json.JSON;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.MultiMap;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+/**
+ * @author z201.coding@gmail.com
+ */
 public class MainVerticle extends AbstractVerticle {
 
   @Override
@@ -17,27 +33,118 @@ public class MainVerticle extends AbstractVerticle {
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
+    String secret = config().getString(ConfKey.GITHUB_SECRET_KEY);
+    JsonArray repositoryArray = config().getJsonArray(ConfKey.GITHUB_REPOSITORY_KEY);
+    Map<String, String> repositoryMap = new HashMap<>();
+    if (null == repositoryArray) {
+      System.out.println("The configuration file github.repository is not set");
+    } else {
+      if (repositoryArray.isEmpty()) {
+        System.out.println("The configuration file github.repository is not set");
+      }
+      repositoryArray.stream().filter(Objects::nonNull).forEach(i -> {
+        String json = i.toString();
+        JsonObject jsonObject = new JsonObject(json);
+        if (jsonObject.isEmpty()) {
+          return;
+        }
+        if (!jsonObject.containsKey(ConfKey.NAME_KEY)) {
+          System.out.println("The configuration file github.repository.name is not set");
+          return;
+        }
+        if (StrUtil.isEmptyIfStr(jsonObject.getString(ConfKey.SSH_URL_KEY))) {
+          System.out.println("The configuration file github.repository.ssh.url is not set");
+          return;
+        }
+        if (StrUtil.isEmptyIfStr(jsonObject.getString(ConfKey.SCRIPT_PATH_KEY))) {
+          System.out.println("The configuration file github.repository.script.path is not set");
+          return;
+        }
+        repositoryMap.put(jsonObject.getString(ConfKey.SSH_URL_KEY), jsonObject.getString(ConfKey.SCRIPT_PATH_KEY));
+      });
+    }
     router
       .post("/info")
       .handler(ctx -> {
         MultiMap multiMap = ctx.request().headers();
-        for (Map.Entry<String, String> stringStringEntry : multiMap) {
-          System.out.println(stringStringEntry.getKey() + " " + stringStringEntry.getValue());
+        String signature = multiMap.get("X-Hub-Signature"); // 被触发的事件的名称
+        JsonObject payloadJson = ctx.getBodyAsJson();
+        if (StrUtil.isEmptyIfStr(signature) || payloadJson.isEmpty()) {
+          ctx.response()
+            .putHeader("Content-Type", "text/plain")
+            .setStatusCode(400);
+          ctx.end("Fail!");
+          return;
         }
-        System.out.println(ctx.getBodyAsJson().toString());
-        ctx.response()
-          .putHeader("Content-Type", "text/plain")
-          .end("hello world!");
+        Boolean state = false;
+        if (payloadJson.containsKey(ConfKey.REPOSITORY_KEY)) {
+          if (payloadJson.getJsonObject(ConfKey.REPOSITORY_KEY).containsKey(ConfKey.GITHUB_REPOSITORY_SHH_URL_KEY)) {
+            String url = payloadJson.getJsonObject(ConfKey.REPOSITORY_KEY).getString(ConfKey.GITHUB_REPOSITORY_SHH_URL_KEY);
+            if (repositoryMap.containsKey(url)) {
+              String getSignature = getSignature(payloadJson.toString(), secret);
+              if (signature.equals(getSignature)) {
+                String path = repositoryMap.get(url);
+                if (FileUtil.exist(path)) {
+                  FileReader fileReader = new FileReader(path);
+                  String content = fileReader.readString();
+                  try {
+                    process(content);
+                    state = true;
+                  } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println(e.getCause());
+                  }
+                }
+              }
+            }
+          }
+        }
+        if (!state) {
+          ctx.response()
+            .putHeader("Content-Type", "text/plain")
+            .setStatusCode(403);
+          ctx.end("Fail!");
+        } else {
+          ctx.response()
+            .putHeader("Content-Type", "text/plain")
+            .end("Succeeded!");
+        }
       });
-    server.requestHandler(router).listen(8888, http -> {
+    server.requestHandler(router).listen(config().getInteger(ConfKey.PORT_KEY, 8888), http -> {
       if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
+        if (StrUtil.isEmptyIfStr(secret)) {
+          startPromise.fail("The configuration file github.webhook.secret is not set");
+        } else {
+          startPromise.complete();
+        }
       } else {
         startPromise.fail(http.cause());
       }
     });
   }
 
+  private void process(String content) throws IOException {
+    List<String> shell = new ArrayList<>();
+    shell.add("sh");
+    shell.add("-c");
+    shell.add(content);
+    ProcessBuilder pb = new ProcessBuilder(shell);
+    Process process = pb.start();
+    String line;
+    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
+    while ((line = reader.readLine()) != null) {
+      System.out.println(line);
+    }
+    reader.close();
+  }
 
+  /**
+   * @param payload
+   * @return
+   */
+  private String getSignature(String payload, String secret) {
+    HMac hMac = SecureUtil.hmacSha1(secret);
+    final byte[] signature = hMac.digest(payload);
+    return "sha1=" + HexUtil.encodeHexStr(signature);
+  }
 }
